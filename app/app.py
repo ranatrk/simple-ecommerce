f746810cfbdfdf40d0b9799e4c4d9d53479ecd3b
ranatrk <rana.t.hassan@gmail.com>
2022-02-04 00:35:05 +0200

    Add create_app function encapsulating flask app

--- app/app.py
+++ app/app.py
@@ -1,26 +1,32 @@
-from gettext import Catalog
 import flask
 from flask import Response, request
+import json
 
-from catalogue import Catalogue
-
-app = flask.Flask(__name__)
+from app.catalogue import Catalogue
 
 catalogue = Catalogue()
 
 
-@app.route("/checkout", methods=["POST"])
-def checkout():
-    """
-    checkout
-    request params: list of watch IDs e.g: ["001","002","001"]
-    """
-    watch_ids_json = request.get_json() or []
-    try:
-        final_price = catalogue.calculate_final_price(watch_ids_json)
-        return {"price": final_price}
-    except KeyError as e:
-        return Response(str(e), status=400)
+def create_app():
+    app = flask.Flask(__name__)
+
+    @app.route("/checkout", methods=["POST"])
+    def checkout():
+        """
+        checkout
+        request params: list of watch IDs e.g: ["001","002","001"]
+        """
+        watch_ids_json = request.get_json() or []
+
+        # watch_ids = json.loads(watch_ids_json)
+        try:
+            final_price = catalogue.calculate_final_price(watch_ids_json)
+            return {"price": final_price}
+        except KeyError as e:
+            return Response(str(e), status=400)
+
+    return app
 
 
-app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    create_app().run(host="0.0.0.0")
